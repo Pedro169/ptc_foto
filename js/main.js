4652f50b980019249e2cf1d587dd1de722494457
pedro169 <pedro.tapiacast@gmail.com>
2024-04-02 07:11:58 -0300

    boton whatsapp

--- js/main.js
+++ js/main.js
@@ -42,3 +42,22 @@ window.addEventListener("click", (event) => {
     modalh.style.display = "none";
   }
 });
+
+// boton para deslizar hacia arriba
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  var scrollBtn = document.getElementById("scrollBtn");
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollBtn.style.display = "block";
+  } else {
+    scrollBtn.style.display = "none";
+  }
+}
+
+function scrollToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
